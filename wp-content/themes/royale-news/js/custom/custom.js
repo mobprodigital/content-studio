ca475bc288650f0e43f2e256a3d087216b6692b0
Gyan Parkash <gyan.p@morrisdigital.mobi>
2018-09-05 10:52:52 +0530

    custom js changes

--- wp-content/themes/royale-news/js/custom/custom.js
+++ wp-content/themes/royale-news/js/custom/custom.js
@@ -1,32 +1,32 @@
 (function ($) {
     var containerEl = document.querySelector('.filteringModeMulti');
+    if (containerEl != null) {
+        var mixer = mixitup(containerEl);
+        var filterCheck = document.querySelectorAll('.filter-check');
+        if (filterCheck.length > 0) {
+            for (var i = 0; i < filterCheck.length; i++) {
+                filterCheck[i].addEventListener('change', function () {
 
-    var mixer = mixitup(containerEl);
-
-
-    var filterCheck = document.querySelectorAll('.filter-check');
-    if (filterCheck.length > 0) {
-        for (var i = 0; i < filterCheck.length; i++) {
-            filterCheck[i].addEventListener('change', function () {
-
-                valueArr = [];
-                for (var j = 0; j < filterCheck.length; j++) {
-                    if (filterCheck[j].checked) {
-                        valueArr.push(filterCheck[j].value);
+                    valueArr = [];
+                    for (var j = 0; j < filterCheck.length; j++) {
+                        if (filterCheck[j].checked) {
+                            valueArr.push(filterCheck[j].value);
+                        }
                     }
-                }
-                mixer.filter(valueArr.length > 0 ? valueArr.join(',') : 'all');
-            });
+                    mixer.filter(valueArr.length > 0 ? valueArr.join(',') : 'all');
+                });
+            }
         }
+
     }
 
 
     // $('.carousel').carousel()
 
-    $(document).ready(function() {
+    $(document).ready(function () {
         $("#lightSlider, #hp-client-slider").lightSlider({
             keyPress: false,
             controls: true,
-        }); 
-      });
+        });
+    });
 }(jQuery))
